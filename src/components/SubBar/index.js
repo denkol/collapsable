b2f0caa4996d29e0a04c38f0272751c726ae842b
Denis <kol9743@gmail.com>
2018-05-08 02:26:15 +0200

    Some fixes of SubBar

--- src/components/SubBar/index.js
+++ src/components/SubBar/index.js
@@ -12,18 +12,33 @@
     ]}
   />
 
+  $(window).on("scroll", function() {
+    var scrollHeight = $(document).height();
+    var scrollPosition = $(window).height() + $(window).scrollTop();
+    if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+      // when scroll to bottom of the page
+    }
+  });
+
 */
 
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import scroll from 'smoothscroll-polyfill';
 import { IconPiBoard, IconMap, IconDrift, IconDashboard, IconTap, IconArrow, IconSmooth, IconCopy } from './icons';
 
 class SubBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isHiddenLeftArrow: true,
+      isHiddenRightArrow: false,
+      scrollPosition: 0
+    };
+
+    this.scrollHandler = this.scrollHandler.bind(this);
   }
   renderSubBarIcon(iconName) {
     switch(iconName) {
@@ -45,22 +60,90 @@ class SubBar extends Component {
         return 'No icon';
     }
   }
+  arrowClickHandler(e, direction) {
+    const { isHiddenLeftArrow, isHiddenRightArrow } = this.state;
+    const contentElem = document.getElementById('sub-bar-content');
+    const viewport = contentElem.offsetWidth;
+    const shiftWidth = viewport / 2;
+    
+    const scrolling = () => {
+      contentElem.scroll({
+        left: this.state.scrollPosition, 
+        behavior: 'smooth' 
+      });
+    }
+
+    if(direction === "left" && isHiddenLeftArrow === false) {
+      this.setState({scrollPosition: this.state.scrollPosition - shiftWidth}, () => {
+        scrolling();
+      });
+    }
+
+    if (direction === "right" && isHiddenRightArrow === false) {
+      this.setState({scrollPosition: this.state.scrollPosition + shiftWidth}, () => {
+        scrolling();
+      });
+    }
+
+    // console.log(this.state.scrollPosition, scrollWidth)
+
+  }
+
+  scrollHandler(e) {
+    const offsetWidth = e.target.offsetWidth;
+    const scrollWidth = e.target.scrollWidth;
+    const scrollLeft = e.target.scrollLeft;
+
+    if ((offsetWidth - scrollLeft) / offsetWidth === 1) {
+      this.setState({
+        isHiddenLeftArrow: true,
+        isHiddenRightArrow: false,
+        scrollPosition: scrollLeft,
+      });
+    } else if (offsetWidth + scrollLeft == scrollWidth) {
+      this.setState({
+        isHiddenLeftArrow: false,
+        isHiddenRightArrow: true,
+        scrollPosition: scrollLeft,
+      });
+    } else {
+      this.setState({
+        isHiddenLeftArrow: false,
+        isHiddenRightArrow: false,
+        scrollPosition: scrollLeft,
+      });
+    }    
+  }
   render() {
+    console.log(this.state)
     const { menuItems, theme } = this.props;
-    // const { } = this.state;
+    const { isHiddenLeftArrow, isHiddenRightArrow} = this.state;
 
     const DynamicClasses = {
       SubBar: classNames({
         'sub-bar': true,
         'sub-bar--theme-dark': theme === "dark",
         'sub-bar--lesser-3': menuItems.length <= 3
-      })
+      }),
+      Arrow: {
+        left: classNames({
+          'sub-bar-arrow': true,
+          'sub-bar-arrow_left': true,
+          'sub-bar-arrow--is-hidden': isHiddenLeftArrow
+        }),
+        right: classNames({
+          'sub-bar-arrow': true,
+          'sub-bar-arrow_right': true,
+          'sub-bar-arrow--is-hidden': isHiddenRightArrow
+        })
+      }
     };
 
     
     return (
       <div className={DynamicClasses.SubBar}>
-        <div className='sub-bar-content'>
+
+        <div className='sub-bar-content' id='sub-bar-content' onScroll={this.scrollHandler}>
           {menuItems.map((item, key) => {
             return (
               <div key={key} className='sub-bar-content__item'>
@@ -72,8 +155,13 @@ class SubBar extends Component {
             );
           })}
         </div>
-        <div className='sub-bar-arrow'>
-          <button className='sub-bar-arrow__icon'>
+        <div className={DynamicClasses.Arrow.left}>
+          <button className='sub-bar-arrow__icon' onClick={(e) => this.arrowClickHandler(e, 'left')}>
+            {IconArrow}
+          </button>
+        </div>
+        <div className={DynamicClasses.Arrow.right}>
+          <button className='sub-bar-arrow__icon' onClick={(e) => this.arrowClickHandler(e, 'right')}>
             {IconArrow}
           </button>
         </div>
